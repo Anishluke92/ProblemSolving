d98a1c998f878a1cf75748ef527679ce41006b74
Anish <anishuke92@gmail.com>
2020-10-16 10:00:10 +0100

    Updated three files, Merged_array,Rockpaperscissors and isthephonenumber

--- Merge_Array.rb
+++ Merge_Array.rb
@@ -16,12 +16,64 @@ merge_arrays(["f", "d", "w", "t"], [5, 3, 7, 8])
 ➞ ["f", 5, "d", 3, "w", 7, "t", 8]
 =end
 
+=begin 
+Create a function that takes two arrays and combines them by alternatingly taking elements from each array in turn.
+
+The arrays may be of different lengths, with at least one character / digit.
+The first array will contain string characters (lowercase, a-z).
+The second array will contain integers (all positive).
+
+Examples
+merge_arrays(["a", "b", "c", "d", "e"], [1, 2, 3, 4, 5])
+➞ ["a", 1, "b", 2, "c", 3, "d", 4, "e", 5]
+
+merge_arrays([1, 2, 3], ["a", "b", "c", "d", "e", "f"])
+➞ [1, "a", 2, "b", 3, "c", "d", "e", "f"]
+
+merge_arrays(["f", "d", "w", "t"], [5, 3, 7, 8])
+➞ ["f", 5, "d", 3, "w", 7, "t", 8]
+=end
+
 def merge_arrays(array1, array2)
-  array1.zip(array2).flatten.compact | array2
+  length = 0
+  arr = []
+
+  length1 =  array1.length
+  length2 =  array2.length
+
+  if length1 == length2
+    for i in 0...length1
+        arr.push(array1[i])
+        arr.push(array2[i])
+    end
+  elsif !(length1 > length2)
+    for i in 0...length2
+      if !array1[i].nil?
+        arr.push(array1[i])
+        arr.push(array2[i])
+      else
+        arr.push(array2[i])
+      end
+    end
+  elsif !(length1 < length2)
+    for i in 0...length1
+      if !array2[i].nil?
+        arr.push(array1[i])
+        arr.push(array2[i])
+      else
+        arr.push(array1[i])
+      end
+    end
+  end
+arr
 end
+
+
 
 print merge_arrays(["a", "b", "c", "d", "e"], [1, 2, 3, 4, 5])
 puts ""
 print merge_arrays([1, 2, 3], ["a", "b", "c", "d", "e", "f"])
 puts ""
 print merge_arrays(["f", "d", "w", "t"], [5, 3, 7, 8])
+puts ""
+print merge_arrays(["a", "b", "c", "d", "e"], [1, 2, 3])
--- RockPaperScissors.rb
+++ RockPaperScissors.rb
@@ -20,46 +20,60 @@ calculate_score([["R", "P"], ["R", "S"], ["S", "P"]]) ➞ "Abigail"
 calculate_score([["R", "R"], ["S", "S"]]) ➞ "Tie"
 
 calculate_score([["S", "R"], ["R", "S"], ["R", "R"]]) ➞ "Tie"
+
 =end
 
-def calculate_score(array)
-  abigail = 0
-  ben = 0
- 
-  array.each do  |game|
-    game.each_with_index do |played, index|
-      
-      if game.include?("R") && game.include?("S")
-        if (index == 0 && played == "R" ) 
-          abigail += 1
-        else
-          ben += 1
-        end
-        
-      elsif game.include?("S") && game.include?("P")
-        if (index == 0 && played == 'S') 
-          abigail += 1 
-        else 
-          ben += 1
-        end
+def who_wins(player1, player2)
 
-      elsif game.include?("P") && game.include?("R")
-        if (index == 0 && played == 'P') 
-          abigail += 1 
-        else
-          ben += 1
-        end
-      else 
+  if player1 > player2
+    "Abigail" 
+  elsif player1 == player2 
+    "Tie"
+  else
+    "Ben"
+  end
+end
+
+def calculate_score(game)
+  abi = 0
+  ben = 0
+
+  if game.length > 0
+    game.each do |element|
+      if (element[0] == 'R') && (element[1] == 'S')
+        abi += 1
+      elsif(element[0]== 'S') && (element[1] == 'P')
+        abi += 1
+      elsif(element[0] == 'P') && (element[1] == 'R')
+        abi += 1
+      elsif(element[0] == 'S') && (element[1] == 'R')
+        ben += 1
+      elsif(element[0] == 'P') && (element[1] == 'S')
+        ben += 1
+      elsif(element[0] == 'R') && (element[1] == 'P')
+        ben += 1
+      elsif ((element[0] == 'R') && (element[1] == 'R')) || ((element[0]== 'S') && (element[1] == 'S')) || ((element[0] == 'P') && (element[1] == 'P'))
+        abi += 0
         ben += 0
-        abigail += 0
       end
     end
-    
+  else
+    return "NO GAME HAS BEEN PLAYED..!!"
   end
- "#{ben}, #{abigail}"
-
+  who_wins(abi, ben)
 end
- 
 
 
-print  calculate_score([["R", "P"], ["S", "R"], ["S", "P"]])
+print calculate_score([["R", "P"], ["R", "S"], ["S", "P"]])
+puts ""
+print calculate_score([["S", "R"], ["R", "S"], ["R", "R"]])
+puts ""
+print calculate_score([["R", "R"], ["S", "S"]])
+puts ""
+print calculate_score([])
+
+
+
+
+
+        
