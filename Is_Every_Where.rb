ef699a4d5d92c52f5026a527053b340f7aa93410
Anishluke92 <anishluke92@gmail.com>
2020-10-30 21:42:10 +0000

    Is_Every_Where.rb coded

--- Is_Every_Where.rb
+++ Is_Every_Where.rb
@@ -12,4 +12,21 @@ isEverywhere([1, 2, 1, 3, 4], 1) → false
 =end
 
 def isEverywhere(array, target)
-    
+    arr = []
+    for i in 0..array.length-2
+        if array[i] == target || array[i+1] == target 
+           arr << 0
+        else
+           arr << 1
+        end
+    end
+    return true if !arr.include?(1)
+    false  
+end
+
+
+print isEverywhere([1, 2, 1, 3], 1)
+puts ""
+print isEverywhere([1, 2, 1, 3], 2)
+puts ""
+print isEverywhere([1, 2, 1, 3, 4], 1)
