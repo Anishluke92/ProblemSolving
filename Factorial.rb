47ba07d1590943747f23e3d9ec11c5af4541431f
Anishluke92 <anishluke92@gmail.com>
2020-10-31 01:26:13 +0000

    Factorial.rb

---
+++ Factorial.rb
@@ -0,0 +1,23 @@
+=begin 
+Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1. Compute the result recursively (without loops).
+factorial(1) → 1
+factorial(2) → 2
+factorial(3) → 6
+=end
+
+def factorial(number)
+    store = 1
+    return 1 if number == 1
+     
+   
+    store *= number
+    return store * factorial(number - 1)
+
+
+end
+ 
+print factorial(1) 
+puts ""
+print factorial(2) 
+puts ""
+print factorial(3)
