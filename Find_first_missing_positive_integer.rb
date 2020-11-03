61e5ec42dacad391247185a7fcb721fa08b0400f
Anishluke92 <anishluke92@gmail.com>
2020-11-03 01:11:29 +0000

    Find_first_missing_positive_integer.rb

--- Find_first_missing_positive_integer.rb
+++ Find_first_missing_positive_integer.rb
@@ -9,20 +9,6 @@ For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should g
 
 You can modify the input array in-place.
 
-
-
-
-
-Try something like this:
-
-Loop through the array.
-Each time calculate what the next number should be. 
-If the next number is now what you expect, then return that number that is missing.
-expected = array[0]
-array.each do |value|
-    return expected if value != expected
-    expected += 1
-end
 =end 
 
 def missingpostiveint(array)
