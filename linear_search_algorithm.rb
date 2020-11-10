3403297e7b46082078bcfeaf9b7f167bf4efacfd
Anishluke92 <anishluke92@gmail.com>
2020-11-10 00:06:48 +0000

    linear_search_algorithm.rb

--- linear_search_algorithm.rb
+++ linear_search_algorithm.rb
@@ -1,8 +1,17 @@
 def linearSearch(array,target)
-    array.each do |item|
-       return item if item == target 
-       return "Not found !"
+  #  array.each do |item|
+  ##     return item if item == target 
+  #     return "Not found !"
+  #  end
+    i = 0 
+    while i < array.length 
+        if array[i] == target 
+            return "Found #{array[i]} at position #{i}"
+        end 
+        i += 1 
     end 
+    "Number not found !"
 end 
+
 
 print linearSearch([1, 3, 6, 8, 12, 14, 15, 20, 142],13)
