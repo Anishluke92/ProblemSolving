755af254ea2343b4ebc4c288ea055a32ab5269c7
Anishluke92 <anishluke92@gmail.com>
2020-12-08 23:55:45 +0000

    SortByLength.rb

--- SortByLength.rb
+++ SortByLength.rb
@@ -17,10 +17,24 @@ then lets do the same to rest of the words in that sentence.
 Now we got to know all the word size in that sentence. Lets rearrange the sentence by sorting the words by size in ascending order. example,
 "my dear hello world"
 =end
-def sort_by_length(strings)
+def sort_algorithm(array,hash)
+  (array.length - 1).times do |x|
+    if array[x] > array[x + 1]
+      array[x], array[x + 1]  = array[x + 1], array[x]
+    end 
+  end 
+  array.map{|number| hash[number]}.join(" ")
+end 
 
-    strings.split(" ").sort_by(&:length).join(" ")
- 
+def sort_by_length(strings)
+  return "No string!" if strings == ""
+  sentence = Hash.new
+  strings = strings.split(" ")
+  strings.each do |word|
+    sentence[word.length] = word
+  end 
+  strings_number = strings.map{|word| word.length}
+  sort_algorithm(strings_number, sentence) 
 end
   
   print sort_by_length("Hello my friend")
@@ -28,3 +42,5 @@ end
   print sort_by_length("Have a wonderful day") 
   puts ""
   print sort_by_length("My son loves pineapples")
+  puts ""
+  print sort_by_length("")
