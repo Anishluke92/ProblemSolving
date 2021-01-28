9c634b21f9697932357ccaa40d5e81f930baa225
Anishluke92 <anishluke92@gmail.com>
2021-01-28 20:12:50 +0000

    The code is simplifed and works- Match_The_bracket.rb

--- Match_The_Bracket.rb
+++ Match_The_Bracket.rb
@@ -7,29 +7,25 @@ For example, given the string "([])", you should return true.
 Given the string "([)]" or "((()", you should return false.
   
 =end
-$brackets = { "{" => "}", "[" => "]", "(" => ")" }
-
-def index_finder(string)
-  reference = ["{", "[", "("]
-  string.chars.each_with_index do |element, index|
-    return index - 1 if !$brackets.has_key?(element)
-  end
-end
 
 def bracket(string)
-  result = []
-  open_side = string[0..index_finder(string)]
-  close_side = string[index_finder(string) + 1..string.length - 1]
-  for i in 0..(open_side.length-1)
-    result << false  if $brackets[open_side[i - 1]] != close_side[i]
-    result << true 
-  end
-  result.all? true
+  brackets = { "{" => "}", "[" => "]", "(" => ")" }
+  array = string.split("")
+  mid = (array.length - 1)/ 2 
+  reverse = 0
+  array.each_with_index do |element, ind|
+    if ind <= mid
+      reverse -= 1
+      return false if brackets[element] != array[reverse] 
+    end 
+  end 
+  true
 end 
-
+ 
 
 print bracket("([])")
 puts ""
 print bracket("((()")
 puts""
 print bracket("([)]")
+
