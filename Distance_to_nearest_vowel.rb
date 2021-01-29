e4c038cb4efb8297794929b60bdcb2586691ef92
Anishluke92 <anishluke92@gmail.com>
2021-01-29 18:41:46 +0000

    Distance_to_nearest_vowel.rb

--- Distance_to_nearest_vowel.rb
+++ Distance_to_nearest_vowel.rb
@@ -20,44 +20,43 @@ Strings will be lowercased.
 Vowels are: a, e, i, o, u.   
 
 =end
-$vowels = { a: 0, e: 0, i: 0, o: 0, u: 0 } 
+$vowels = { a: 0, e: 0, i: 0, o: 0, u: 0 }
 
-def distance_calculation(array, ind)
-   right_count = 0
-   left_count= 0
-   (0..ind).each do |i|
-     right_count = array.index(array[ind - i]) + 1  if  $vowels[array[ind - i].to_sym]
-   end
-   (ind..array.length - 1).each do |i|
-     left_count = array.index(array[i]) + 1 if  $vowels[array[i].to_sym]
-   end 
- 
-   
+def calculation(array, index)
+  left_count = 0 
+  right_count = 0
+  for i in 0..index
+    break if $vowels[array[index - i].to_sym]
+    left_count += 1
+  end   
+  for j in index..array.length-1
+    break if $vowels[array[j].to_sym]
+    right_count += 1
+  end 
 
-       
-
-    
-end 
+  return left_count if index == array.length - 1
+  return right_count if left_count >= right_count || index == 0  
+  left_count
+end
 
 def distance_to_nearest_vowel(string)
-    result = [] 
-   # vowels = { a: 0, e: 0, i: 0, o: 0, u: 0 } 
-    array = string.chars
-    array.each_with_index do |letter, ind|
-        if !$vowels[letter.to_sym]
-            result << distance_calculation(array, ind)
-        else 
-            result << $vowels[letter.to_sym]
-        end
-    end 
-    result
+  result = [] 
+  array = string.chars
+  array.each_with_index do |letter, ind|
+      if !$vowels[letter.to_sym]
+          result << calculation(array, ind)
+      else 
+          result << $vowels[letter.to_sym]
+      end
+  end 
+  result
 end 
 
-#print distance_to_nearest_vowel("aaaaa")
+print distance_to_nearest_vowel("aaaaa")
 puts " "
 print distance_to_nearest_vowel("abcdabcd")
 puts " "
-#print distance_to_nearest_vowel("babbb") 
+print distance_to_nearest_vowel("babbb") 
 puts " "
-#print distance_to_nearest_vowel("shopper")
+print distance_to_nearest_vowel("shopper")
 puts " "
