1fca36dbf9e2fbd901ac205d373dae6a3206ec01
Anishluke92 <anishluke92@gmail.com>
2021-01-31 18:30:58 +0000

    Code modified

--- Population_is_a_prime_number.rb
+++ Population_is_a_prime_number.rb
@@ -1,6 +1,9 @@
 =begin
-2. A positive number's population is the number of 1s in its binary representation. An evil number has an even numbered population, whereas an odious number has an odd numbered population. Moreover, a number is pernicious if its population is a prime number.
-Create a function that takes a number as an argument and returns a sorted array of all its descriptors ("Evil", "Odious", or "Pernicious").
+2. A positive number's population is the number of 1s in its binary representation. 
+An evil number has an even numbered population, whereas an odious number has an odd numbered population.
+ Moreover, a number is pernicious if its population is a prime number.
+Create a function that takes a number as an argument and
+ returns a sorted array of all its descriptors ("Evil", "Odious", or "Pernicious").
 Examples
 how_bad(7) ➞ ["Odious", "Pernicious"]
 # binary = 111
@@ -9,17 +12,25 @@ how_bad(17) ➞ ["Evil", "Pernicious"]
 how_bad(23) ➞ ["Evil"]
 # binary = 10111
 =end
+require 'prime'
 
 def how_bad(n)
-  numbered_population = ["Evil" , "Odious","Pernicious"]
+  numbered_population = ["Evil", "Odious", "Pernicious"]
   result = []
   binary = n.to_s(2).count('1')
+  
   if binary % 2 == 0 
-    return result.push(numbered_population[0]) if binary != 2
-    result.push(numbered_population[0],numbered_population[2])
+    if !Prime.prime?(binary)
+      result.push(numbered_population[0])
+    else 
+      result.push(numbered_population[0],numbered_population[2])
+    end
   else 
-    return result.push(numbered_population[1]) if binary == 1 
+    if !Prime.prime?(binary)
+      result.push(numbered_population[1])
+    else 
     result.push(numbered_population[1],numbered_population[2])
+    end 
   end
 end
 
