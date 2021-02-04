1c6ddcef67f47a1985851710b83b322a7e4ca46a
Anishluke92 <anishluke92@gmail.com>
2021-02-04 14:12:01 +0000

    Queue/Simple_Queue.rb

---
+++ Queue/Simple_Queue.rb
@@ -0,0 +1,57 @@
+=begin
+Simple Queue  Data structure 
+Idea - FiFo
+=end
+
+class Queue 
+  attr_reader :queues
+  
+  def initialize
+    @queues = []
+  end
+
+  def enqueue(item)
+    @queues.unshift(item)
+  end 
+
+  def dequeue
+    @queues.pop
+  end 
+
+  def size 
+    @queues.size 
+  end 
+
+  def display 
+    @queues
+  end 
+
+  def peek 
+    @queues[-1]
+  end 
+end 
+
+
+queues = Queue.new 
+queues.enqueue("q")
+queues.enqueue(1)
+queues.enqueue(2)
+queues.enqueue(3)
+queues.enqueue(4)
+queues.enqueue(10)
+
+print queues.size
+puts ""
+print queues.display
+puts ""
+print queues.peek
+puts ""
+print queues.dequeue
+puts ""
+print queues.display 
+
+
+
+
+
+
