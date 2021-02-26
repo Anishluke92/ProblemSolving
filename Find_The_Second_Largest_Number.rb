941542c1804a96565eacafda10a4f4c616ac8678
Anishluke92 <anishluke92@gmail.com>
2021-02-26 13:05:35 +0000

    Algorithm built and code fixed

--- Find_The_Second_Largest_Number.rb
+++ Find_The_Second_Largest_Number.rb
@@ -10,17 +10,19 @@ second_largest([25, 143, 89, 13, 105]) ➞ 105
 second_largest([54, 23, 11, 17, 10]) ➞ 23
 =end
 
-
 def second_largest(array)
-  a = 1
-  first_largest = array.max
   highest = 0
-  array.each_with_index do |number, ind|   
-    highest = number  if highest < number && number != first_largest
-    return highest if ind == array.length - 1
+  second_largest = 0
+  array.each_with_index do |number, index|
+    if highest < number
+      second_largest = highest
+      highest = number 
+    else
+      second_largest = number if second_largest < number 
+    end 
+    return second_largest if index == array.length - 1
   end 
-end 
-
+end
 
 print second_largest([10, 40, 30, 20, 50])
 puts " "
