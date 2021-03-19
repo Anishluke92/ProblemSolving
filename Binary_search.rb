cc1aa860f28e0aa2ad4b4087b8244ce0a8706bcb
Anishluke92 <anishluke92@gmail.com>
2021-03-19 15:14:27 +0000

    Code improved and indented properly

--- Binary_search.rb
+++ Binary_search.rb
@@ -1,20 +1,19 @@
 def binary_search(array, target)
-    low = 0
-    high = array.length - 1 
-    while low <= high 
-        middleindex = ( low + high ) >> 1
-       if target > array[middleindex]
-        low = middleindex + 1
-        middleindex = ( low + high ) >> 1
-       elsif  target < array[middleindex]
-        high = middleindex - 1
-        middleindex = ( low + high ) >> 1
-       else
-        return "Number #{target} found at position #{middleindex + 1}"
-       end
+  low = 0
+  high = array.length - 1 
+  while low <= high 
+    middleindex = ( low + high ) 
+    if target > array[middleindex]
+      low = middleindex + 1
+      middleindex = ( low + high ) 
+    elsif  target < array[middleindex]
+      high = middleindex - 1
+      middleindex = ( low + high ) 
+    else
+      return "Number #{target} found at position #{middleindex + 1}"
     end
-    "Number not found !"
-    
+  end
+    "Number not found !"  
 end 
 
-print binary_search([1, 5, 7, 30, 51, 55, 67, 88, 89, 100, 102], 0)
+print binary_search([1, 5, 7, 30, 51, 55, 67, 88, 89, 100, 102], )
